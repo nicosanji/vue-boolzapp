0649cac043f4367205a19b36b04ac6a6e24ade1b
Nicolò Sangiorgio <n.sangiorgio@hotmail.it>
2021-11-04 12:26:01 +0100

    Milestone 3 completata

---
+++ milestone 3/script.js
@@ -0,0 +1,142 @@
+// MILESTONE 1
+// 1) Replica della grafica con la possibilità di avere messaggi 
+//    scritti dall’utente (verdi) e dall’interlocutore (bianco) 
+//    assegnando due classi CSS diverse.
+// 2) Visualizzazione dinamica della lista contatti: tramite la 
+//    direttiva v-for, visualizzare nome e immagine di ogni contatto.
+// MILESTONE 2
+// 3) Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, 
+//    visualizzare tutti i messaggi relativi al contatto attivo  
+//    all’interno del pannello della conversazione.
+// 4) Click sul contatto mostra la conversazione del contatto cliccato.
+// MILESTONE 3
+// 5) Aggiunta di un messaggio: l’utente scrive un testo nella parte 
+//    bassa e digitando “enter” il testo viene aggiunto al thread
+//    sopra, come messaggio verde.
+// 6) Risposta dall’interlocutore: ad ogni inserimento di un messaggio,
+//    l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+/////////////////////////////////////////////////////////////////////
+
+// Istanza di VueJs
+Vue.config.devtools = true;
+new Vue({
+    el: "#root",
+    // DATA
+    data: {
+        index: 0,
+        myMessage: "",
+        contacts: [
+            // Michele
+            {
+                name: 'Michele',
+                avatar: '_1',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Ricordati di dargli da mangiare',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 16:15:22',
+                        text: 'Tutto fatto!',
+                        status: 'received'
+                    }
+                ],
+            },
+            // Fabio
+            {
+                name: 'Fabio',
+                avatar: '_2',
+                visible: true,
+                messages: [
+                    {
+                        date: '20/03/2020 16:30:00',
+                        text: 'Ciao come stai?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '20/03/2020 16:30:55',
+                        text: 'Bene grazie! Stasera ci vediamo?',
+                        status: 'received'
+                    },
+                    {
+                        date: '20/03/2020 16:35:00',
+                        text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                        status: 'sent'
+                    }
+                ],
+            },
+            // Samuele
+            {
+                name: 'Samuele',
+                avatar: '_3',
+                visible: true,
+                messages: [
+                    {
+                        date: '28/03/2020 10:10:40',
+                        text: 'La Marianna va in campagna',
+                        status: 'received'
+                    },
+                    {
+                        date: '28/03/2020 10:20:10',
+                        text: 'Sicuro di non aver sbagliato chat?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '28/03/2020 16:15:22',
+                        text: 'Ah scusa!',
+                        status: 'received'
+                    }
+                ],
+            },
+            // Luisa
+            {
+                name: 'Luisa',
+                avatar: '_4',
+                visible: true,
+                messages: [
+                    {
+                        date: '10/01/2020 15:30:55',
+                        text: 'Lo sai che ha aperto una nuova pizzeria?',
+                        status: 'sent'
+                    },
+                    {
+                        date: '10/01/2020 15:50:00',
+                        text: 'Si, ma preferirei andare al cinema',
+                        status: 'received'
+                    }
+                ],
+            },
+        ]
+    },
+    // METHODS
+    methods: {
+        // invia un messaggio
+        sendMyMessage(index) {
+            // day.js
+            let timestamp = `${new Date().toLocaleDateString().substr(0, 10)} ${new Date().toLocaleTimeString()}`
+            this.contacts[index].messages.push({
+                date: timestamp,
+                text: this.myMessage,
+                status: 'sent'
+            }),
+                // risposta automatica
+                setTimeout(() => {
+                    // day.js
+                    let timestamp = `${new Date().toLocaleDateString().substr(0, 10)} ${new Date().toLocaleTimeString()}`
+                    this.contacts[index].messages.push({
+                        date: timestamp,
+                        text: 'ok',
+                        status: 'received'
+                    });
+                }, 2000);
+            this.myMessage = "";
+        }
+    }
+})
